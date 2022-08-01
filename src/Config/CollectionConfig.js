7dac19279ef85e97dfb1097c14e4207b3a6f44a2
James Iacabucci <iacabucci@outlook.com>
2022-07-31 17:07:43 -0700

    new contract and deployment

--- src/Config/CollectionConfig.js
+++ src/Config/CollectionConfig.js
@@ -1,6 +1,6 @@
 const CollectionConfig = {
   contractName: 'DeixaXastroCollection',
-  contractAddress: '0x52B336939441C96a5710ff49dF1b674d023aC5ff',
+  contractAddress: '0x96D126599cD2789269Ab0176CF389CC36424F0c6',
   openSeaURL: 'opensea.io/collection/deixa-xastro-origin',
 };
 
